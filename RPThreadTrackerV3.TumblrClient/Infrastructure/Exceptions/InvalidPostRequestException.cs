ad03124b77eff25805054659d36c24eb100e2f49
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2018-02-07 10:34:13 -0600

    Removing Tumblrclient

--- RPThreadTrackerV3.TumblrClient/Infrastructure/Exceptions/InvalidPostRequestException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RPThreadTrackerV3.TumblrClient.Infrastructure.Exceptions
-{
-	using System;
-
-	public class InvalidPostRequestException : Exception
-    {
-		public InvalidPostRequestException() : base("Invalid request for post information.") { }
-    }
-}
--- RPThreadTrackerV3.TumblrClient/Infrastructure/Exceptions/PostNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RPThreadTrackerV3.TumblrClient.Infrastructure.Exceptions
-{
-	using System;
-
-	public class PostNotFoundException : Exception
-    {
-		public PostNotFoundException() : base("The requested post does not exist.") { }
-    }
-}
