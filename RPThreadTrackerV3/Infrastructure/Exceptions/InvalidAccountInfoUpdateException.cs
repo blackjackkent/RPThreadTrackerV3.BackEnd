03dfe9331686c202a013c73009b4a88044a66e54
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2018-04-10 22:13:29 -0500

    CHange username/email

---
+++ RPThreadTrackerV3/Infrastructure/Exceptions/InvalidAccountInfoUpdateException.cs
@@ -0,0 +1,14 @@
+﻿namespace RPThreadTrackerV3.Infrastructure.Exceptions
+{
+	using System;
+	using System.Collections.Generic;
+
+	public class InvalidAccountInfoUpdateException : Exception
+    {
+	    public List<string> Errors { get; }
+
+	    public InvalidAccountInfoUpdateException(List<string> errors) : base("There was an error updating the users's account information.") {
+		    Errors = errors;
+	    }
+	}
+}
