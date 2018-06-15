a835ae76c17b2380ccf0d4e1e6a6d32e31df494a
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2018-06-15 15:50:23 -0500

    Refactoring base repository tests

---
+++ RPThreadTrackerV3.BackEnd.Test/TestHelpers/Data/MockEntityNavigationProperty.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="MockEntityNavigationProperty.cs" company="Rosalind Wills">
+// Copyright (c) Rosalind Wills. All rights reserved.
+// Licensed under the GPL v3 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace RPThreadTrackerV3.BackEnd.Test.TestHelpers.Data
+{
+    using Interfaces.Data;
+
+    public class MockEntityNavigationProperty : IEntity
+    {
+        public string MockEntityNavigationPropertyId { get; set; }
+    }
+}
---
+++ RPThreadTrackerV3.BackEnd.Test/TestHelpers/Data/MockTrackerContext.cs
@@ -0,0 +1,13 @@
+﻿namespace RPThreadTrackerV3.BackEnd.Test.TestHelpers
+{
+    using BackEnd.Infrastructure.Data;
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+
+    public class MockTrackerContext : TrackerContext
+    {
+        public virtual DbSet<MockEntityPoco> MockEntityPocos { get; set; }
+
+        public virtual DbSet<MockEntityNavigationProperty> MockEntityNavigationProperties { get; set; }
+    }
+}
