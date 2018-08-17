9728530f26573f5222dec47c5f1602d65c80b8c9
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2018-08-16 23:47:53 -0500

    More travis fixes

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ DOCKER_PASSWORD=$3
 echo "$TAG"
 echo "$DOCKER_USERNAME"
 echo "$DOCKER_PASSWORD"
-docker build -t $DOCKER_USERNAME/rpthreadtrackerv3.backend .
+docker build -t blackjacksoftware/rpthreadtrackerv3.backend .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push $DOCKER_USERNAME/rpthreadtrackerv3.backend:latest
