491802b242e5b0e4abc9a0b03d01dfe293ee6950
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2018-08-16 23:36:33 -0500

    Checking build process

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ TAG=$1
 DOCKER_USERNAME=$2
 DOCKER_PASSWORD=$3
 
+echo "$TAG"
+echo "$DOCKER_USERNAME"
+echo "$DOCKER_PASSWORD"
 docker build -t $DOCKER_USERNAME/rpthreadtrackerv3.backend .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push $DOCKER_USERNAME/rpthreadtrackerv3.backend:latest
