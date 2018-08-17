6390b4390bba4447eb8a48241783c5305711dbc5
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2018-08-16 23:15:56 -0500

    Adding test controller

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ TAG=$1
 DOCKER_USERNAME=$2
 DOCKER_PASSWORD=$3
 
-docker-compose build
+docker build -t $DOCKER_USERNAME/rpthreadtrackerv3.backend .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push $DOCKER_USERNAME/rpthreadtrackerv3.backend:latest
