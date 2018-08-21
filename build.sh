6f186fa6e648e1660d2ca716449405e052f8fcba
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2018-08-21 01:33:37 -0500

    Adding swagger file to csproj

--- build.sh
+++ build.sh
@@ -4,7 +4,10 @@ set -ev
 dotnet restore
 dotnet build -c Release
 # test
+# linux
 dotnet test "RPThreadTrackerV3.BackEnd.Test/RPThreadTrackerV3.BackEnd.Test.csproj" -c Release /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+# windows
+# dotnet test "RPThreadTrackerV3.BackEnd.Test/RPThreadTrackerV3.BackEnd.Test.csproj" -c Release //p:CollectCoverage=true //p:CoverletOutputFormat=opencover
 wget "https://codecov.io/bash" -O codecov.sh
 chmod a+x ./codecov.sh
 ./codecov.sh -f "RPThreadTrackerV3.BackEnd.Test/coverage.opencover.xml" 
